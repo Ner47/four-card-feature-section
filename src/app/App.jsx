e53fa85f6db23f4b59ead9f8ae7dab9527869c11
Ner47 <neogil28@gmail.com>
2026-02-10 12:44:19 +0300

    feat: add start base

---
+++ src/app/App.jsx
@@ -0,0 +1,22 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <>
+      <main className={styles.page}>
+        <header className={styles.page_header}>
+          <h1>
+            <span>Reliable, efficient delivery</span>
+            <br /> Powered by Technology
+          </h1>
+          <p>
+            Our Artificial Intelligence powered tools use millions of project data points to ensure
+            that your project is successful
+          </p>
+        </header>
+      </main>
+    </>
+  );
+}
+
+export default App;
